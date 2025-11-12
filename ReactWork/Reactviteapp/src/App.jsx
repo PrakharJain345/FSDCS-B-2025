cc6791da7242213d96c1a778e4230d5a3173d424
PrakharJain345 <jkavita357@gmail.com>
2025-11-12 10:29:10 +0530

    react dom and form handling

--- ReactWork/Reactviteapp/src/App.jsx
+++ ReactWork/Reactviteapp/src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
+import Registration from './pages/Registration'
+import Dashboard from './pages/Dashboard'
+import MainLayout from './pages/MainLayout'
+import { useState } from 'react'
 
 function App() {
+  const [data,setData]=useState();
+  
+  
+
   return (
     <div>
       {/* <h2>Welcome to ReactVite.</h2> */}
@@ -13,9 +21,16 @@ function App() {
 
        <BrowserRouter>
        <Routes>
-        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/login" element={<Login logData={data}/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/registration" element={<Registration regData={setData}/>}></Route>
+        <Route path="/" element={<MainLayout/>}></Route>
        </Routes>
        </BrowserRouter>
+
+       <h2>
+        {JSON.stringify(data)}
+       </h2>
 
   
     </div>
--- ReactWork/Reactviteapp/src/Pages/Login.jsx
+++ ReactWork/Reactviteapp/src/Pages/Login.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Login() {
+function Login({logData}) {
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+
+  function verification(e) {
+    e.preventDefault();
+    console.log(logData);
+    // alert(email + " " + password);
+//     if (email === logData.email) {
+//       if (password === logData.password) {
+//         alert("Login Successful");
+//       } else {
+//         alert("Invalid Password");
+//       }
+//     } else {
+//       alert("User not registered");
+//     }
+  }
+
   return (
     <div>
       <h2>Login Form</h2>
       <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input onChange={(e) => setemail(e.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input onChange={(e) => setpassword(e.target.value)} type="password" className="form-control" id="exampleInputPassword1" />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <button onClick={verification}  className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
